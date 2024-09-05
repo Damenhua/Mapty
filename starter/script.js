bcd6186450c5330b3a72e3c39f84b85f174acf9c
Jeffrey <Damenhua@gmail.com>
2024-09-05 18:05:00 +0800

    create Leaflet map, and use mark

--- starter/script.js
+++ starter/script.js
@@ -10,3 +10,45 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+      //"map" 對應到html中，id = "map"
+      // 創建 Leaflet 地圖，並將視圖設定為使用者的座標
+      const coords = [latitude, longitude];
+      const map = L.map('map').setView(coords, 22);
+
+      // 加入 OpenStreetMap 圖層
+      L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      // 在使用者的位置上加一個標記
+      map.on('click', function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      });
+    },
+
+    function () {
+      alert('Could not get your position');
+    }
+  );
